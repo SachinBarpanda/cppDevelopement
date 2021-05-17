d41f3a71aa36403120ac07de6230ecebc6a93a74
SachinBarpanda <sachinongithub@gmail.com>
2021-05-17 10:37:08 +0530

    bit

--- Learning/Introduction/accessingHeapMemory.c
+++ Learning/Introduction/accessingHeapMemory.c
@@ -4,7 +4,7 @@ int main(){
     
     p = (int) malloc(5*sizeof(int));//1. 5 is the size of array 
     //2. we give the compiler to decide the size-of the int 
-    //3. then we type cast it to (int) at first place
+    //3. then we type cast it to (int*)  at first place as it returns void 
     
     //Thus an array of size 5 is created in heap
     
