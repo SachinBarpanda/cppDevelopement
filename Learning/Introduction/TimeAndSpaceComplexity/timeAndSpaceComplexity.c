ed94c5b2fc269b92cb5d1d7dde86c4aead1aa6ff
SachinBarpanda <sachinongithub@gmail.com>
2021-05-28 23:54:52 +0530

    learning time and space complexity

--- Learning/Introduction/TimeAndSpaceComplexity/timeAndSpaceComplexity.c
+++ Learning/Introduction/TimeAndSpaceComplexity/timeAndSpaceComplexity.c
@@ -1,21 +1,25 @@
 #include<stdio.h>
 int main(){
     //declaring an array 
-    int arr[5] = {5,3,6,7,1,19};
+    int sumInArray = 0;
+    int arr[6] = {5,3,6,7,1,19};
     //declaring a matrix or a 2d array
+    int sumInMatrix[3] = {0,0,0,0};
     int mat[3][3] = {{4,3},{2,3},{6,5},{8,10}};
 
     //parsing this takes o(n) time complexity
     for ( int i = 0; i < 6; i++)
     {
+        sumInArray =  sumInArray+arr[i];
         /* code */
-        printf("%d ",arr[i]);
+        printf("%d ",sumInArray);
     }   
     printf("\n");
     //parsing this takes o(n^2) time complexity
     for(int i=0; i<4;i++){
         for(int j = 0;j < 4;j++){
-            printf("%d ",mat[i][j]);
+            sumInMatrix[i] = sumInMatrix[i]+mat[i][j];
+            printf("%d ",sumInMatrix[i]);
         }
         printf("\n");
     }
