4119926635a6ae029cf110b812054a684884249a
SachinBarpanda <sachinongithub@gmail.com>
2021-10-27 21:47:11 +0530

    multi dimen array

--- OwnPractice/ProgrammingBasics/Pointers/b.cpp
+++ OwnPractice/ProgrammingBasics/Pointers/b.cpp
@@ -1,13 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
-void increment(int* a){
-    ++a;
-    *a = 5;
+void increment(int* a,int *b){
+    (*a)++;
+    // *a = 5;
     
 }
 
 int main(){
     int a = 2;
-    increment(&a);
-    cout << a;
+    int b = 4;
+    increment(&a, &b);
+    cout << a ;
 }
