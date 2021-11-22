5ecb024005723ddfa4d5e90fd5c0bd9bb84426c8
SachinBarpanda <sachinongithub@gmail.com>
2021-11-22 09:42:16 +0530

    right quick

--- OwnPractice/ProgrammingBasics/VectorCpp/quickSort.cpp
+++ OwnPractice/ProgrammingBasics/VectorCpp/quickSort.cpp
@@ -8,20 +8,26 @@ void swap(int arr[], int i, int j){
 
 int partition(int arr[], int l , int r){
     int pivot = arr[r];
-    int i = l-1;
-    for(int j = 0 ; j < r-1  ; j++){
+    int i = l;
+    for(int j = l ; j < r ; j++){
         if(arr[j]<pivot){
-            i++;
+            
             swap(arr, i, j);
+            i++;
         }
     }
-    swap(arr, i+1, r);
-    return i+1;
+    swap(arr, i, r);
+
+    cout << "Iteration CHecking : ";
+    for(int i = 0 ; i < 8 ; i++){
+        cout<< arr[i] << " ";    
+    }
+    cout << "pi : " <<r<<endl;
+    return i;
     
 }
 
 void quickSort(int arr[], int l, int r){
-    
     
     if(l<r){
         int pivot = partition(arr,l,r);
